2eb2d103658493e0cc569b2453df2911ce1e171a
itsMar-IA-na <mi.lopeza@uea.edu.ec>
2025-03-19 09:42:55 -0500

    s14.py

--- s14.py
+++ s14.py
@@ -1,5 +1,5 @@
 # Definir la función
-def calculo_desc(valor_total, valor_desc):
+def calcular_descuento(valor_total, valor_desc):
 # Calcular el valor del descuento
     descuento = valor_total * (valor_desc / 100)
 # Devolver el valor del descuento ya calculado
@@ -8,14 +8,14 @@ def calculo_desc(valor_total, valor_desc):
 # Llamar a la función con el valor de la compra y el porcentaje de descuento
 valor_total_1 = 145.67  # Asignar valor a la compra
 valor_desc_1 = 17  # Asignar valor al descuento
-descuento_1 = calculo_desc(valor_total_1, valor_desc_1)  # Llamar a la función calculo_desc con valor_total_1 y valor_desc_1, y almacena el descuento calculado en descuento_1.
+descuento_1 = calcular_descuento(valor_total_1, valor_desc_1)  # Llamar a la función calculo_desc con valor_total_1 y valor_desc_1, y almacena el descuento calculado en descuento_1.
 valor_final_1 = valor_total_1 - descuento_1  # Calcular el monto final a pagar restando el descuento del valor total.
 
 # Se repiten los pasos
 # Llamar a la función con el valor de la compra y el porcentaje de descuento
 valor_total_2 = 564.78  # Asignar valor a la compra
 valor_desc_2 = 13  # Asignar valor al descuento
-descuento_2 = calculo_desc(valor_total_2, valor_desc_2)  # Llamar a la Función calculo_desc con valor_total_2 y valor_desc_2, y almacena el descuento calculado en descuento_2.
+descuento_2 = calcular_descuento(valor_total_2, valor_desc_2)  # Llamar a la Función calculo_desc con valor_total_2 y valor_desc_2, y almacena el descuento calculado en descuento_2.
 valor_final_2 = valor_total_2 - descuento_2  # Calcular el Valor Final a Pagar
 
 # Mostrar los resultados con 3 decimales y el porcentaje de descuento
